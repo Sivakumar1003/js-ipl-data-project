dd57568c2a12256d2a704b8513993ae666f16ea9
Sivakumar N <shivask94423@gmail.com>
2025-01-09 10:46:34 +0530

    To get match won by per team per year.

--- src/server/matchWonPerTeamPerYear.js
+++ src/server/matchWonPerTeamPerYear.js
@@ -4,26 +4,20 @@ const data = makeObject('./src/data/matches.csv');
 
 // function to get match won by per year per team.
 function matchWonPerTeamPerYear(data) {
-    let teamsResult = {};
-    for (let match of data) {
+
+    return data.reduce((seasonResult, match) => {
         let winner = match["winner"];
-        let year = match["season"];
 
-        // check winner is there or draw match.
+        // check the match is draw or won.
         if (winner) {
-            if (teamsResult[year] === undefined) {
-                teamsResult[year] = {}
-                teamsResult[year][winner] = 1;
-            } else if (teamsResult[year][winner] == undefined) {
-                teamsResult[year][winner] = 1;
-            } else {
-                teamsResult[year][winner] += 1;
+            if (seasonResult[match["season"]] === undefined) {
+                seasonResult[match["season"]] = {};
             }
+
+            seasonResult[match["season"]][winner] === undefined ?
+                seasonResult[match["season"]][winner] = 1 : seasonResult[match["season"]][winner] += 1;
         }
-    }
-
-    // returning teamsResult.
-    return teamsResult;
+        return seasonResult;
+    }, {})
 }
-
 fs.writeFileSync('./src/public/output/matchWonPerTeamPerYear.json', JSON.stringify(matchWonPerTeamPerYear(data)));
